eba7fd038adc973ec8213111cb7673b6e3bceff6
Harbiinger <theogodin51@gmail.com>
2021-05-31 16:18:01 +0200

    tui - interface only

---
+++ tui.py
@@ -0,0 +1,57 @@
+import npyscreen
+
+class MainForm(npyscreen.ActionFormV2):
+    def create(self):
+        self.addChannel = self.add(npyscreen.ButtonPress, name="Add", when_pressed_function=self.goToAdd)
+        self.deleteChannel = self.add(npyscreen.ButtonPress, name="Delete", when_pressed_function=self.goToDel)
+        self.webhook = self.add(npyscreen.ButtonPress, name="Edit webhook", when_pressed_function=self.goToWebhook)
+
+    def goToAdd(self):
+        self.parentApp.switchForm('ADD')
+
+    def goToDel(self):
+        self.parentApp.switchForm('DEL')
+
+    def goToWebhook(self):
+        self.parentApp.switchForm('WEBHOOK')
+
+    def on_cancel(self):
+        self.parentApp.switchForm(None)
+        
+
+class AddForm(npyscreen.ActionForm):
+    def create(self):
+        self.channelName = self.add(npyscreen.TitleText, name="Name:")
+        self.channelId= self.add(npyscreen.TitleText, name="Id:")
+
+    def on_cancel(self):
+        self.channelName.value = ""
+        self.channelId.value = ""
+        self.parentApp.setNextForm('MAIN')
+
+class DelForm(npyscreen.ActionForm):
+    def create(self):
+        self.channelName = self.add(npyscreen.TitleText, name="Name:")
+
+    def on_cancel(self):
+        self.channelName.value = ""
+        self.parentApp.setNextForm('MAIN')
+
+class WebhookForm(npyscreen.ActionForm):
+    def create(self):
+        self.webhook = self.add(npyscreen.TitleText, name="Webhook url:")
+
+    def on_cancel(self):
+        self.webhook.value = ""
+        self.parentApp.setNextForm('MAIN')
+
+class App(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.addForm('MAIN', MainForm, name="Ungoogled Youtube Subscriptions")
+        self.addForm('ADD', AddForm, name="Ungoogled Youtube Subscriptions")
+        self.addForm('DEL', DelForm, name="Ungoogled Youtube Subscriptions")
+        self.addForm('WEBHOOK', WebhookForm, name="Ungoogled Youtube Subscriptions")
+
+    
+if __name__ == '__main__':
+    mainApp = App().run()
